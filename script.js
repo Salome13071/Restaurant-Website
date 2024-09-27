7bebe9739012bdec90e4f18d202141f28d8cb199
Salome Karseladze <saly13071@gmail.com>
2024-09-27 16:33:21 +0400

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let stakeSlider = {
+  red: {
+    name: "sss",
+    image: "asdasd",
+    color: "red",
+    desc: "sasd",
+  },
+  green: {
+    name: "sss",
+    image: "asdasd",
+    color: "green",
+    desc: "sasd",
+  },
+};
